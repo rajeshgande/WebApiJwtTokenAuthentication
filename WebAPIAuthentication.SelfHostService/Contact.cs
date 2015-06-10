ec4670431dc3b3ed7fb0c0cbf27ec3756e989373
unknown <rajeshg@OMCL764-DDQKP32.omnicell.com>
2015-06-10 08:57:29 -0700

    added OAuth authentication

--- WebAPIAuthentication.SelfHostService/Contact.cs
+++ WebAPIAuthentication.SelfHostService/Contact.cs
@@ -1,8 +1,47 @@
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Identity;
+
 namespace WebAPIAuthentication.SelfHostService
 {
     public class Contact
     {
         public string Name { get; set; }
         public string Phone { get; set; }
+    }
+
+    public class ApplicationUser : Microsoft.AspNet.Identity.IUser<string>
+    {
+       
+
+        public IList<Claim> Claims { get; set; }
+        public string Id { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+
+
+        public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager, string authenticationType)
+        {
+            var userIdentity = await manager.CreateIdentityAsync(this, authenticationType);
+            // Add custom user claims here
+
+            return userIdentity;
+        }
+    }
+
+    public class ApplicationUserManager : UserManager<ApplicationUser>
+    {
+        IList<ApplicationUser>  _users = new List<ApplicationUser>()
+        {
+            new ApplicationUser(){Id="1", UserName="UserOne", Password = "UserOne"},
+            new ApplicationUser(){Id="2", UserName="UserTwo", Password = "UserTwo"}
+        };
+
+        public ApplicationUser Find(string _userId, string _password)
+        {
+            return _users.FirstOrDefault(x => x.Id == _userId && x.Password == _password);
+        }
     }
 }
---
+++ WebAPIAuthentication.SelfHostService/Providers/CustomOAuthProvider.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Identity.Owin;
+using Microsoft.Owin.Security;
+using Microsoft.Owin.Security.OAuth;
+
+namespace WebAPIAuthentication.SelfHostService.Providers
+{
+    public class CustomOAuthProvider : OAuthAuthorizationServerProvider
+    {
+
+        public override Task ValidateClientAuthentication(OAuthValidateClientAuthenticationContext context)
+        {
+            context.Validated();
+            return Task.FromResult<object>(null);
+        }
+
+        public override async Task GrantResourceOwnerCredentials(OAuthGrantResourceOwnerCredentialsContext context)
+        {
+
+            var allowedOrigin = "*";
+
+            context.OwinContext.Response.Headers.Add("Access-Control-Allow-Origin", new[] { allowedOrigin });
+
+            var userManager = context.OwinContext.GetUserManager<ApplicationUserManager>();
+
+            ApplicationUser applicationUser =  userManager.Find(context.UserName, context.Password);
+
+            if (applicationUser == null)
+            {
+                context.SetError("invalid_grant", "The ApplicationUser name or password is incorrect.");
+                return;
+            }
+            
+            ClaimsIdentity oAuthIdentity = await applicationUser.GenerateUserIdentityAsync(userManager, "JWT");
+
+            var ticket = new AuthenticationTicket(oAuthIdentity, null);
+
+            context.Validated(ticket);
+
+        }
+    }
+}
