9f76055ede90ebea633907bf8f34f3d41135cf6f
unknown <rajeshg@OMCL764-DDQKP32.omnicell.com>
2015-06-10 07:50:27 -0700

    working self host webapi serivce

---
+++ WebAPIAuthentication.SelfHostService/ContactController.cs
@@ -0,0 +1,82 @@
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+
+namespace WebAPIAuthentication.SelfHostService
+{
+    public class ContactController : ApiController
+    {
+        // Mock a data store:
+        private static List<Contact> _contacts = new List<Contact>
+        {
+            new Contact { Name = "Rajesh", Phone= "2515543900" },
+            new Contact { Name = "Dave", Phone= "111-111-1111" },
+            new Contact { Name = "Henry", Phone= "222-222-2222" }
+        };
+
+
+        public IEnumerable<Contact> Get()
+        {
+            return _contacts;
+        }
+
+
+        public Contact Get(string name)
+        {
+            var Contact = _contacts.FirstOrDefault(c => c.Name == name);
+            if (Contact == null)
+            {
+                throw new HttpResponseException(System.Net.HttpStatusCode.NotFound);
+            }
+            return Contact;
+        }
+
+
+        public IHttpActionResult Post(Contact Contact)
+        {
+            if (Contact == null)
+            {
+                return BadRequest("Argument Null");
+            }
+            var ContactExists = _contacts.Any(c => c.Name == Contact.Name);
+
+            if (ContactExists)
+            {
+                return BadRequest("Exists");
+            }
+
+            _contacts.Add(Contact);
+            return Ok();
+        }
+
+
+        public IHttpActionResult Put(Contact Contact)
+        {
+            if (Contact == null)
+            {
+                return BadRequest("Argument Null");
+            }
+            var existing = _contacts.FirstOrDefault(c => c.Name == Contact.Name);
+
+            if (existing == null)
+            {
+                return NotFound();
+            }
+
+            existing.Name = Contact.Name;
+            return Ok();
+        }
+
+
+        public IHttpActionResult Delete(string name)
+        {
+            var Contact = _contacts.FirstOrDefault(c => c.Name == name);
+            if (Contact == null)
+            {
+                return NotFound();
+            }
+            _contacts.Remove(Contact);
+            return Ok();
+        }
+    }
+}
---
+++ WebAPIAuthentication.SelfHostService/Startup.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Http;
+using Owin;
+
+namespace WebAPIAuthentication.SelfHostService
+{
+    public class Startup
+    {
+        // This method is required by Katana:
+        public void Configuration(IAppBuilder app)
+        {
+            var webApiConfiguration = ConfigureWebApi();
+
+            // Use the extension method provided by the WebApi.Owin library:
+            app.UseWebApi(webApiConfiguration);
+        }
+
+        private HttpConfiguration ConfigureWebApi()
+        {
+            var config = new HttpConfiguration();
+            config.Routes.MapHttpRoute(
+                "DefaultApi",
+                "api/{controller}/{name}",
+                new { name = RouteParameter.Optional });
+            return config;
+        }
+    }
+}
